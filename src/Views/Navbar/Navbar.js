db94060f3cf2aeb037dcd069a334c550348072ff
vaibhavgaikwad8476 <rajnandinitikkal@gmail.com>
2023-11-13 17:48:20 +0530

    add nav

--- src/Views/Navbar/Navbar.js
+++ src/Views/Navbar/Navbar.js
@@ -5,6 +5,13 @@ export default function Navbar() {
   return (
     <>
     <nav className="navbar-container">
+      <div className='nav-Heading'>
+      <div className='nav-items'>Home</div>
+      <div className='nav-items'>About</div>
+      <div className='nav-items'>Contact</div>
+      <div className='nav-items'>Login</div>
+      <div className='nav-items'>SignUp</div>
+      </div>
     </nav>
     </>
   )
