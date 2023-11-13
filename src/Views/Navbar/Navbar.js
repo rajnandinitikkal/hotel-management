d092ccea60a177327d5aa9f263ee64251983ad45
vaibhavgaikwad8476 <rajnandinitikkal@gmail.com>
2023-11-13 14:20:49 +0530

    add navbar

---
+++ src/Views/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Navbar.css"
+
+export default function Navbar() {
+  return (
+    <>
+    <nav className="navbar-container">
+    </nav>
+    </>
+  )
+}
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-
+import Navbar from './Views/Navbar/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <></>
+   <>
+   <Navbar/>
+   </>
 );
 
 
